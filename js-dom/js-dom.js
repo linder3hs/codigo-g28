d814bab1b305f76ab1e88a0a982b435dda7c46fb
linder3hs <linderhassinger00@gmail.com>
2025-11-14 20:04:53 -0500

    js-dom

--- js-dom/js-dom.js
+++ js-dom/js-dom.js
@@ -17,3 +17,25 @@ tituloPrincipal.textContent = "Codigo G28";
 // classList.add: sirve para agregar una clase a una etiqueta
 // classList.remove: sirve para eliminar una clase a una etiqueta
 tituloPrincipal.classList.add("css-titulo-principal");
+
+// document.querySelector: busca 1 element
+// document.querySelectorAll: busca todas las coincidencias
+
+// nombre de etiqueta
+const parrafo = document.querySelector("p");
+const parrafos = document.querySelectorAll("p");
+console.log(parrafo);
+console.log(parrafos);
+
+const btnOne = document.querySelector(".btn-one");
+console.log(btnOne);
+
+const contenedor = document.querySelector("#contenedor");
+
+contenedor.innerHTML = `
+<h2>Titulo de JS</h2>
+<img width="200" src="https://yt3.googleusercontent.com/MqAUGsICRxKJmJAYlrqUnqKLGlQ5hzaiBJKbaqLSLIcGYh9fwf6kcU4i3BgmWG5o-y7W9j1Bx3Q=s900-c-k-c0x00ffffff-no-rj" />
+<div>
+  <p>Hola mundo</p>
+</div>
+`;
