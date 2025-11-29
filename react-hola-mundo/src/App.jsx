7537feb4708b6373cf0d544f78a8a5567d544ebe
linder3hs <linderhassinger00@gmail.com>
2025-11-28 20:16:05 -0500

    useState

--- react-hola-mundo/src/App.jsx
+++ react-hola-mundo/src/App.jsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+
 function App() {
+  const [taskName, setTaskName] = useState("");
+
+  function handleInput(event) {
+    // console.log(event.target.value);
+    setTaskName(event.target.value);
+  }
+
   return (
     <div className="h-screen bg-gray-100 px-6 py-8 space-y-10">
       <h1 className="text-center text-3xl font-bold">TodoList</h1>
@@ -7,6 +16,8 @@ function App() {
           <input
             type="text"
             placeholder="Nombre de la tarea"
+            value={taskName}
+            onChange={handleInput}
             className="border border-gray-400 px-3 py-2 rounded-l flex-1 outline-none"
           />
           <button
