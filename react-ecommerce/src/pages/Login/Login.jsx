fbc8cc3b10f7f38bdd5a429e3ad35d471461f149
linder3hs <linderhassinger00@gmail.com>
2025-12-11 21:11:23 -0500

    react login

--- react-ecommerce/src/pages/Login/Login.jsx
+++ react-ecommerce/src/pages/Login/Login.jsx
@@ -1,7 +1,9 @@
+import { LoginForm } from "@/components/login-form";
+
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="h-screen flex flex-col justify-center p-6 max-w-lg mx-auto">
+      <LoginForm />
     </div>
   );
 }
